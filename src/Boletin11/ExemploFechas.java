7670cb455708e8452ed814701ba2417f1b91bd53
agarridogarcia <agarridogarcia@nautilus5.danielcastelao.org>
2016-12-01 11:49:05 +0100

    2

--- src/Boletin11/ExemploFechas.java
+++ src/Boletin11/ExemploFechas.java
@@ -12,7 +12,7 @@ public class ExemploFechas {
    
     public static void main(String[] args) {
     Fechas obx1=new Fechas();
-     obx1.fechaActual();
+    obx1.fechaActual();
     obx1.calcularTempo();
    
        
