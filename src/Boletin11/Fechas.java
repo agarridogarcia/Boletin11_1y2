52b5b79052c9b861eaf45772edb39132cf6cb322
agarridogarcia <agarridogarcia@nautilus5.danielcastelao.org>
2016-12-02 11:33:44 +0100

    6

--- src/Boletin11/Fechas.java
+++ src/Boletin11/Fechas.java
@@ -10,9 +10,9 @@ import javax.swing.JOptionPane;
 
 public class Fechas {
     public void calcularTempo (){
-        Instant ahora1=Instant.now();
+        Instant ahora1=Instant.now();//tempo no que se empeza a escribir
         String resposta=JOptionPane.showInputDialog("Escribe a seguinte: \n A documentación é todo aquel \n conxunto de manuais impresos \n ou en formato dixital que \n explique unha aplicación \n informática .");
-        Instant antes1=Instant.now();
+        Instant antes1=Instant.now();//tempo no que acabamos de escribir
         System.out.println("Tardaste em escribir a frase: " + Duration.between(ahora1, antes1).getSeconds() + "segundos");
         
     }
@@ -26,6 +26,8 @@ public class Fechas {
        System.out.printf("Fai seis meses foi %s%n", LocalDateTime.now().minusMonths(6)); 
        System.out.printf("Fai seis dias foi %s%n", LocalDateTime.now().minusDays(6)); 
        System.out.printf("Fai cinco semanas foi %s%n", LocalDateTime.now().minusWeeks(5));
+       
+       
     }
     
 }
